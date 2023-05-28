bb8df76583e59e2463ff332dd7e10fa7c7a84119
BojayJaya <95891943+BojayJaya@users.noreply.github.com>
2023-05-29 01:40:22 +0700

    Update pba.py

--- pba.py
+++ pba.py
@@ -7,9 +7,23 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-# from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.tree import DecisionTreeClassifier
+import nltk
+nltk.download('punkt')
+import string 
+import re
+from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+import swifter
+import pickle
+import ast
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 st.title("PEMROSESAN BAHASA ALAMI A")
 st.write("### Dosen Pengampu : Dr. FIKA HASTARITA RACHMAN, ST., M.Eng")
@@ -19,7 +33,7 @@ st.write("##### Pramudya Dwi Febrianto - 200411100042")
 st.write("##### Febrian Achmad Syahputra - 200411100106")
 
 #Navbar
-data_set_description, upload_data, preprocessing, modeling, implementation = st.tabs(["Data Set Description", "Upload Data", "Preprocessing", "Modeling", "Implementation"])
+data_set_description, upload_data, preprocessing, ekstraksifitur, modeling, implementation = st.tabs(["Data Set Description", "Upload Data", "Preprocessing", "Modeling", "Implementation"])
 
 df = pd.read_csv('https://raw.githubusercontent.com/HambaliFitrianto/Aplikasi-Web-Data-Mining-Weather-Prediction/main/seattle-weather.csv')
 
@@ -98,119 +112,239 @@ with upload_data:
         st.header("Dataset")
         st.dataframe(df)
 
-#Preprocessing
 with preprocessing:
-    st.subheader("""Normalisasi Data""")
-    st.write("""Rumus Normalisasi Data :""")
-    st.image('https://i.stack.imgur.com/EuitP.png', use_column_width=False, width=250)
-    st.markdown("""
-    Dimana :
-    - X = data yang akan dinormalisasi atau data asli
-    - min = nilai minimum semua data asli
-    - max = nilai maksimum semua data asli
-    """)
-    df = df.drop(columns=["date"])
-    #Mendefinisikan Varible X dan Y
-    X = df.drop(columns=['weather'])
-    y = df['weather'].values
-    df_min = X.min()
-    df_max = X.max()
+    st.subheader("Preprocessing Data")
     
-    #NORMALISASI NILAI X
-    scaler = MinMaxScaler()
-    #scaler.fit(features)
-    #scaler.transform(features)
-    scaled = scaler.fit_transform(X)
-    features_names = X.columns.copy()
-    #features_names.remove('label')
-    scaled_features = pd.DataFrame(scaled, columns=features_names)
+    st.write("Case Folding:")
+    dataset['ulasan'] = dataset['ulasan'].str.lower()
+    st.write("Case Folding Result:")
+    st.write(dataset['ulasan'].head())
 
-    st.subheader('Hasil Normalisasi Data')
-    st.write(scaled_features)
+    st.write("Tokenize:")
+    def hapus_tweet_khusus(text):
+        text = text.replace('\\t', " ").replace('\\n', " ").replace('\\u', " ").replace('\\', "")
+        text = text.encode('ascii', 'replace').decode('ascii')
+        text = ' '.join(re.sub("([@#][A-Za-z0-9]+)|(\w+:\/\/\S+)", " ", text).split())
+        return text.replace("http://", " ").replace("https://", " ")
+    
+    dataset['ulasan'] = dataset['ulasan'].apply(hapus_tweet_khusus)
+    
+    def hapus_nomor(text):
+        return re.sub(r"\d+", "", text)
+    
+    dataset['ulasan'] = dataset['ulasan'].apply(hapus_nomor)
+    
+    def hapus_tanda_baca(text):
+        return text.translate(str.maketrans("", "", string.punctuation))
+    
+    dataset['ulasan'] = dataset['ulasan'].apply(hapus_tanda_baca)
+    
+    def hapus_whitespace_LT(text):
+        return text.strip()
+    
+    dataset['ulasan'] = dataset['ulasan'].apply(hapus_whitespace_LT)
+    
+    def hapus_whitespace_multiple(text):
+        return re.sub('\s+', ' ', text)
+    
+    dataset['ulasan'] = dataset['ulasan'].apply(hapus_whitespace_multiple)
+    
+    def hapus_single_char(text):
+        return re.sub(r"\b[a-zA-Z]\b", "", text)
+    
+    dataset['ulasan'] = dataset['ulasan'].apply(hapus_single_char)
+    
+    def word_tokenize_wrapper(text):
+        tokenizer = RegexpTokenizer(r'dataran\s+tinggi|jawa\s+tengah|[\w\']+')
+        tokens = tokenizer.tokenize(text)
+        return tokens
+    
+    dataset['ulasan_tokens'] = dataset['ulasan'].apply(word_tokenize_wrapper)
+    
+    def freqDist_wrapper(text):
+        return FreqDist(text)
+    
+    dataset['ulasan_tokens_fdist'] = dataset['ulasan_tokens'].apply(freqDist_wrapper)
+    
+    st.write("Filtering (Stopword Removal):")
+    list_stopwords = stopwords.words('indonesian')
+    list_stopwords.extend(["yg", "dg", "rt", "dgn", "ny", "d", 'klo', 'kalo', 'amp', 'biar', 'bikin', 'bilang',
+                        'gak', 'ga', 'krn', 'nya', 'nih', 'sih', 'si', 'tau', 'tdk', 'tuh', 'utk', 'ya',
+                        'jd', 'jgn', 'sdh', 'aja', 'n', 't', 'nyg', 'hehe', 'pen', 'u', 'nan', 'loh', 'rt',
+                        '&amp', 'yah'])
+    
+    txt_stopword = pd.read_csv("https://raw.githubusercontent.com/masdevid/ID-Stopwords/master/id.stopwords.02.01.2016.txt", names=["stopwords"], header=None)
+    
+    list_stopwords.extend(txt_stopword["stopwords"][0].split(' '))
+    list_stopwords = set(list_stopwords)
+    
+    def stopwords_removal(words):
+        return [word for word in words if word not in list_stopwords]
 
-    st.subheader('Target Label')
-    dumies = pd.get_dummies(df.weather).columns.values.tolist()
-    dumies = np.array(dumies)
+    dataset['ulasan_tokens_WSW'] = dataset['ulasan_tokens'].apply(stopwords_removal)
 
-    labels = pd.DataFrame({
-        '1' : [dumies[0]],
-        '2' : [dumies[1]],
-        '3' : [dumies[2]],
-        '4' : [dumies[3]],
-        '5' : [dumies[4]]
-    })
+    st.write("Stemming:")
+    factory = StemmerFactory()
+    stemmer = factory.create_stemmer()
 
-    st.write(labels)
+    def stemmed_wrapper(term):
+        return stemmer.stem(term)
 
-#Modelling
+    term_dict = {}
+
+    for document in dataset['ulasan_tokens_WSW']:
+        for term in document:
+            if term not in term_dict:
+                term_dict[term] = ' '
+
+    print(len(term_dict))
+    print("------------------------")
+
+    for term in term_dict:
+        term_dict[term] = stemmed_wrapper(term)
+        print(term, ":", term_dict[term])
+
+    print(term_dict)
+    print("------------------------")
+
+    def get_stemmed_term(document):
+        return [term_dict[term] for term in document]
+
+    dataset['ulasan_tokens_stemmed'] = dataset['ulasan_tokens_WSW'].apply(get_stemmed_term)
+
+    print(dataset['ulasan_tokens_stemmed'])
+
 with modeling:
-    training, test = train_test_split(scaled_features,test_size=0.2, random_state=1)#Nilai X training dan Nilai X testing
-    training_label, test_label = train_test_split(y, test_size=0.2, random_state=1)#Nilai Y training dan Nilai Y testing
-    with st.form("modeling"):
-        st.subheader('Modeling')
-        st.write("Pilihlah model yang akan dilakukan pengecekkan akurasi:")
-        # naive = st.checkbox('Gaussian Naive Bayes')
-        k_nn = st.checkbox('K-Nearest Neighboor')
-        # destree = st.checkbox('Decission Tree')
-        submitted = st.form_submit_button("Submit")
+    st.write("Menyimpan data hasil preprocessing ke pickle")
+    with open('data.pickle', 'wb') as file:
+        pickle.dump(dataset, file)
 
-        #KNN
-        K=5
-        knn=KNeighborsClassifier(n_neighbors=K)
-        knn.fit(training,training_label)
-        knn_predict=knn.predict(test)
+    # Memuat data dari file pickle
+    with open('data.pickle', 'rb') as file:
+        loaded_data = pickle.load(file)
+    Data_ulasan = pd.DataFrame(loaded_data, columns=["label", "ulasan"])
+    Data_ulasan.head()
+    ulasan = Data_ulasan['ulasan']
+    sentimen = Data_ulasan['label']
+    X_train, X_test, y_train, y_test = train_test_split(ulasan, sentimen, test_size=0.2, random_state=42)
 
-        knn_akurasi = round(100 * accuracy_score(test_label,knn_predict))
+    def convert_text_list(texts):
+        try:
+            texts = ast.literal_eval(texts)
+            if isinstance(texts, list):
+                return texts
+            else:
+                return []
+        except (SyntaxError, ValueError):
+            return []
 
-        if submitted :
-            if k_nn :
-                st.write("Model KNN accuracy score : {0:0.2f}" . format(knn_akurasi))
-                
-        grafik = st.form_submit_button("Grafik akurasi semua model")
-        if grafik:
-            data = pd.DataFrame({
-                'Akurasi' : [knn_akurasi],
-                'Model' : ['KNN'],
-            })
+    Data_ulasan["ulasan_list"] = Data_ulasan["ulasan"].apply(convert_text_list)
+    print(Data_ulasan["ulasan_list"][90])
+    print("\ntype: ", type(Data_ulasan["ulasan_list"][90]))
 
-            chart = (
-                alt.Chart(data)
-                .mark_bar()
-                .encode(
-                    alt.X("Akurasi"),
-                    alt.Y("Model"),
-                    alt.Color("Akurasi"),
-                    alt.Tooltip(["Akurasi", "Model"]),
-                )
-                .interactive()
-            )
-            st.altair_chart(chart,use_container_width=True)
+    # Ekstraksi fitur menggunakan TF-IDF
+    def calculate_tf(corpus):
+        tf_dict = {}
+        for document in corpus:
+            words = document.split()
+            for word in words:
+                if word not in tf_dict:
+                    tf_dict[word] = 1
+                else:
+                    tf_dict[word] += 1
+        total_words = sum(tf_dict.values())
+        for word in tf_dict:
+            tf_dict[word] = tf_dict[word] / total_words
+        return tf_dict
 
-#Implementasi
+    def calculate_df(corpus):
+        df_dict = {}
+        for document in corpus:
+            words = set(document.split())
+            for word in words:
+                if word not in df_dict:
+                    df_dict[word] = 1
+                else:
+                    df_dict[word] += 1
+        return df_dict
+
+    def calculate_idf(corpus):
+        idf_dict = {}
+        N = len(corpus)
+        df_dict = calculate_df(corpus)
+        for word in df_dict:
+            idf_dict[word] = np.log(N / df_dict[word])
+        return idf_dict
+
+    def calculate_tfidf(tf_dict, idf_dict):
+        tfidf_dict = {}
+        for word in tf_dict:
+            if word in idf_dict:
+                tfidf_dict[word] = tf_dict[word] * idf_dict[word]
+            else:
+                tfidf_dict[word] = 0
+        return tfidf_dict
+
+    tf_train = calculate_tfidf(calculate_tf(X_train), calculate_idf(X_train))
+    tf_test = calculate_tfidf(calculate_tf(X_test), calculate_idf(X_train))
+
+    print("Term Frequency-Inverse Document Frequency (TF-IDF):")
+    for i, document in enumerate(X_train):
+        tfidf_dict = calculate_tfidf(calculate_tf([document]), calculate_idf(X_train))
+        print(f"Document {i+1}:")
+        for word, tfidf in tfidf_dict.items():
+            print(f"{word}: {tfidf}")
+        print()
+
+    def text_to_vector(text, tfidf_dict):
+        words = text.split()
+        vector = np.zeros(len(tfidf_dict))
+        for i, word in enumerate(tfidf_dict):
+            if word in words:
+                vector[i] = tfidf_dict[word]
+        return vector
+
+    X_train_vectors = [text_to_vector(document, tf_train) for document in X_train]
+    X_test_vectors = [text_to_vector(document, tf_test) for document in X_test]
+
+    print("Vector Representation (TF-IDF):")
+    for i, vector in enumerate(X_train_vectors):
+        print(f"Document {i+1}: {vector}")
+    print()
+
+    # Klasifikasi menggunakan KNN
+    knn_classifier = KNeighborsClassifier()
+    knn_classifier.fit(X_train_vectors, y_train)
+
+# Implementasi dengan Streamlit
 with implementation:
-    with st.form("my_form"):
-        st.subheader("Implementasi")
-        Ulasan = st.number_input('Masukkan Ulasan anda : ')
-        model = st.selectbox('Pilihlah model yang akan anda gunakan untuk melakukan prediksi?',
-                ('KNN'))
+    st.title("Klasifikasi Sentimen Ulasan Menggunakan KNN")
+    st.write("Masukkan ulasan di bawah ini:")
+    input_text = st.text_input("Ulasan")
 
-        prediksi = st.form_submit_button("Submit")
-        if prediksi:
-            inputs = np.array([
-                Ulasan,
-            ])
+    if st.button("Prediksi"):
+        # Mengubah input ulasan menjadi vektor
+        input_vector = text_to_vector(input_text, tf_train)
 
-            df_min = X.min()
-            df_max = X.max()
-            input_norm = ((inputs - df_min) / (df_max - df_min))
-            input_norm = np.array(input_norm).reshape(1, -1)
+        # Melakukan prediksi pada input ulasan
+        predicted_label = knn_classifier.predict([input_vector])
 
-            if model == 'KNN':
-                mod = knn 
+        # Menampilkan hasil prediksi
+        st.write("Hasil Prediksi:")
+        st.write(f"Ulasan: {input_text}")
+        st.write(f"Label: {predicted_label[0]}")
 
-            input_pred = mod.predict(input_norm)
+    # Menghitung akurasi pada data uji
+    y_pred = knn_classifier.predict(X_test_vectors)
+    accuracy = accuracy_score(y_test, y_pred)
 
-            st.subheader('Hasil Prediksi')
-            st.write('Menggunakan Pemodelan :', model)
+    # Menampilkan akurasi
+    st.write("Akurasi: {:.2f}%".format(accuracy * 100))
 
-            st.write(input_pred)
+    # Menampilkan label prediksi
+    st.write("Label Prediksi:")
+    for i, (label, ulasan) in enumerate(zip(y_pred, X_test)):
+        st.write(f"Data Uji {i+1}:")
+        st.write(f"Ulasan: {ulasan}")
+        st.write(f"Label: {label}")
+        st.write()
