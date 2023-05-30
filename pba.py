39517716a98a4f8aaee1b1048dc097b29c34430d
BojayJaya <95891943+BojayJaya@users.noreply.github.com>
2023-05-30 09:11:09 +0700

    Update pba.py

--- pba.py
+++ pba.py
@@ -146,12 +146,12 @@ if submit:
 
     # Menampilkan hasil prediksi
     st.write("Hasil Prediksi:")
-    st.write(f"Ulasan: {ulasan}")
-    st.write(f"Label: {predicted_label[0]}")
+#     st.write(f"Ulasan: {ulasan}")
+#     st.write(f"Label: {predicted_label[0]}")
 
     # Menghitung akurasi pada data uji
-#     y_pred = knn_classifier.predict(X_test_vectors)
-#     accuracy = accuracy_score(y_test, y_pred)
+    y_pred = knn_classifier.predict(X_test_vectors)
+    accuracy = accuracy_score(y_test, y_pred)
 
     # Menampilkan akurasi
     st.write("Akurasi: {:.2f}%".format(accuracy * 100))
