6d37f230259b2042ee1adb76c76fcacd4eeef4ba
BojayJaya <95891943+BojayJaya@users.noreply.github.com>
2023-05-30 08:42:59 +0700

    Update pba.py

--- pba.py
+++ pba.py
@@ -196,9 +196,9 @@ if submit:
 
     for i, document in enumerate(X_train):
         tfidf_dict = calculate_tfidf(calculate_tf([document]), calculate_idf(X_train))
-#         st.write(f"Document {i+1}:")
+        st.write(f"Document {i+1}:")
         for word, tfidf in tfidf_dict.items():
-#             st.write(f"{word}: {tfidf}")
+            st.write(f"{word}: {tfidf}")
 
     def text_to_vector(text, tfidf_dict):
         words = text.split()
